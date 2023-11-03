bab5525cbc3f825580751701b4ffa026926cb662
Sergio <64045896+sfacilazaro@users.noreply.github.com>
2023-11-03 12:39:07 +0100

    Update image.py

--- image.py
+++ image.py
@@ -7,16 +7,9 @@ Created on Wed Sep  6 10:41:42 2023
 """
 
 import numpy as np
-#import scipy as sp
-import matplotlib.pyplot as plt
-#import seaborn as sb
-#import sklearn as skl
 import tensorflow as tf
-import pandas as pd
 import os
 import cv2
-import sys
-import subprocess
 
 def acc(pred,targ):
     rows, cols = np.shape(targ-pred)
